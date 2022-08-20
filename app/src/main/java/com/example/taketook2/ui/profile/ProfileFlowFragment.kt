6a2a0e98fe6d3b340126c7b017a4013906010e42
Yana Glad <55493845+YanaGlad@users.noreply.github.com>
2022-08-20 22:22:16 +0300

    SignInFragment works. Added base ripple effect.

---
+++ app/src/main/java/com/example/taketook2/ui/profile/ProfileFlowFragment.kt
@@ -0,0 +1,42 @@
+package com.example.taketook2.ui.profile
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.taketook2.R
+import com.example.taketook2.databinding.FragmentProfileBinding
+import com.example.taketook2.databinding.FragmentProfileFlowBinding
+
+/**
+ * @author y.gladkikh
+ */
+class ProfileFlowFragment : Fragment() {
+
+    private var _binding: FragmentProfileFlowBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        _binding = FragmentProfileFlowBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+            parentFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainer, SignInFragment())
+            .addToBackStack(null)
+            .commit();
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
--- app/src/main/java/com/example/taketook2/ui/profile/SignInFragment.kt
+++ app/src/main/java/com/example/taketook2/ui/profile/SignInFragment.kt
@@ -6,15 +6,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.taketook2.R
+import com.example.taketook2.databinding.FragmentRegistrationBinding
+import com.example.taketook2.databinding.FragmentSignInBinding
 
 
 class SignInFragment : Fragment() {
 
+    private var _binding: FragmentSignInBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_in, container, false)
+    ): View {
+        _binding = FragmentSignInBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
