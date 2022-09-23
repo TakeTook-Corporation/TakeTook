f9167e2aed540206c6e863a9dd7b325f929f7352
Yana Glad <55493845+YanaGlad@users.noreply.github.com>
2022-09-23 20:22:02 +0300

    Fragments for listings

---
+++ app/src/main/java/com/example/taketook2/ui/services/listings/listinginfo/ListingInfoFragment.kt
@@ -0,0 +1,24 @@
+package com.example.taketook2.ui.services.listings.listinginfo
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.taketook2.R
+
+class ListingInfoFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_listing_info, container, false)
+    }
+}
