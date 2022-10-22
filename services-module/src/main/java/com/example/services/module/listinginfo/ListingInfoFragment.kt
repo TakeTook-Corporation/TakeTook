98337752e33098fc857b057ca55c5dec886b061e
Yana Glad <55493845+YanaGlad@users.noreply.github.com>
2022-10-22 19:56:52 +0300

    Listing item xml

--- services-module/src/main/java/com/example/services/module/listinginfo/ListingInfoFragment.kt
+++ services-module/src/main/java/com/example/services/module/listinginfo/ListingInfoFragment.kt
@@ -6,22 +6,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.services.module.R
+import com.example.services.module.databinding.FragmentListingInfoBinding
+import com.example.services.module.databinding.FragmentServicesBinding
 
 /*
  * @author y.gladkikh
  */
 class ListingInfoFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
+    private var _binding: FragmentListingInfoBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_listing_info, container, false)
+    ): View {
+        _binding = FragmentListingInfoBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
